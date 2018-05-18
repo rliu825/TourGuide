6dabc08e45d9b908d599ba68871d2d3f962fb882
Ermin Wang <erminwang1@gmail.com>
2018-05-17 22:30:05 -0700

    working version

--- src/main/java/com/guide/tour/Application.java
+++ src/main/java/com/guide/tour/Application.java
@@ -2,12 +2,13 @@ package com.guide.tour;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
-@EnableAutoConfiguration
+@SpringBootApplication
 class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
